2afb563be1da93fd45f8ca6318eec9b368c3bdb6
revilorasec <cesar.oliver@gmail.com>
2026-08-29 21:15:59 -0300

    feat: adiciona configuração Microsoft do Portal

---
+++ lib/auth-config.ts
@@ -0,0 +1,7 @@
+export const microsoftAuth = {
+  clientId: '88cf5cba-9f67-467d-8a51-9638200bed52',
+  tenantId: '911e1aee-070e-421b-ae71-439f01c2263e',
+  scopes: ['User.Read'],
+} as const;
+
+export const bootstrapAdminEmail = 'cesar.oliver@livionsolutions.com.br';
